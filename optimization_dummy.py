90bd16d87bea8ed1eb6af19e8f2f0d409dda519b
Mees W <mees@famwesseling.nl>
2024-09-20 11:22:27 +0200

    made different functions, made a loop for running more times, made different programs for the experiments

--- optimization_dummy.py
+++ optimization_dummy.py
@@ -26,6 +26,10 @@ def simulation(env,x):
     f,p,e,t = env.play(pcont=x)
     return f
 
+def simulation_indu(env,x):
+    f,p,e,t = env.play(pcont=x)
+    return f,p,e,t
+
 # evaluation
 def evaluate(env, x):
     return np.array(list(map(lambda y: simulation(env,y), x)))
@@ -33,7 +37,7 @@ def evaluate(env, x):
 
 def main():
     # choose this for not using visuals and thus making experiments faster
-    headless = True
+    headless = False
     if headless:
         os.environ["SDL_VIDEODRIVER"] = "dummy"
 
@@ -42,17 +46,21 @@ def main():
     if not os.path.exists(experiment_name):
         os.makedirs(experiment_name)
 
-    # Parameters for evolutionary algorithm
+    # Parameters for EA
     n_hidden_neurons = 10
     dom_l = -1
     dom_u = 1
-    npop = 20
-    cx_prob = 0.8  # Probability of mating (crossover)
-    mut_prob = 0.02  # Probability of mutating
-    n_generations = 10  # Number of generations
-    tournsize = 5
+    npop = 100
+    cx_prob = 0.7  # Probability of mating (crossover)
+    mut_prob = 0.05  # Probability of mutating
+    n_generations = 20  # Number of generations
+    tournsize = 2
 
-    random.seed(43) #43 shows a nice graph
+
+    run_times = 10
+    program_name = "run_solution"
+
+ #   random.seed(43) #43 shows a nice graph
 
 
     # initializes simulation in individual evolution mode, for single static enemy.
@@ -65,13 +73,102 @@ def main():
                     speed="fastest",
                     visuals=True)
 
+
+    env.state_to_log()
+
+    # Running the
+    if program_name == "test_single":
+        logbook = run_ea(env, n_hidden_neurons, dom_l, dom_u, npop, cx_prob, mut_prob, n_generations, tournsize)
+
+        # Plot the fitness values over generations
+        gen = logbook.select("gen")
+        avg = logbook.select("avg")
+        std = logbook.select("std")
+        min_fitness = logbook.select("min")
+        max_fitness = logbook.select("max")
+
+        plt.figure(figsize=(10, 6))
+        plt.plot(gen, avg, label="Average Fitness")
+        plt.fill_between(gen, np.array(avg) - np.array(std), np.array(avg) + np.array(std), alpha=0.2, label="Standard Deviation")
+        plt.plot(gen, min_fitness, label="Minimum Fitness", color="red")
+        plt.plot(gen, max_fitness, label="Maximum Fitness", color="green")
+        plt.xlabel("Generation")
+        plt.ylabel("Fitness")
+        plt.title("Fitness Evolution Over Generations")
+        plt.legend(loc="best")
+        plt.grid(True)
+        plt.show()
+
+    # Running the ea run_times and mapping the avg en std in a graph
+    elif program_name == "run_experiment":
+
+        # Make empty arrays
+        all_avg_fitness = np.zeros((run_times, n_generations))
+        all_std_fitness = np.zeros((run_times, n_generations))
+        all_max_fitness = np.zeros((run_times, n_generations))
+        best_fitness_all = 0
+
+
+        # Runs ea run_times times
+        for run in range(run_times):
+            print(f"Running simulation {run + 1}/{run_times}")
+            logbook, current_best_individual, best_fitness= run_ea(env, n_hidden_neurons, dom_l, dom_u, npop, cx_prob, mut_prob, n_generations, tournsize)
+            if best_fitness > best_fitness_all:
+                best_fitness_all = best_fitness
+                best_individual = current_best_individual
+
+            all_avg_fitness[run, :] = logbook.select("avg")
+            all_std_fitness[run, :] = logbook.select("std")
+            all_max_fitness[run, :] = logbook.select("max")
+
+        # saves file with the best solution
+        np.savetxt(experiment_name + '/best.txt', best_individual)
+        print(f"The best solutions fitness is {best_fitness_all} and has been saved")
+
+        # Calculates the total avg and std
+        avg_fitness_across_runs = np.mean(all_avg_fitness, axis=0)
+        std_fitness_across_runs = np.mean(all_std_fitness, axis=0)
+        max_fitness_across_runs = np.mean(all_max_fitness, axis=0)
+
+
+        # Plot the avg and std
+        generations = list(range(n_generations))
+        plt.figure(figsize=(10, 6))
+        plt.plot(generations, avg_fitness_across_runs, label="Average Fitness")
+        plt.plot(generations, max_fitness_across_runs, label="Max Fitness")
+        plt.fill_between(generations, avg_fitness_across_runs - std_fitness_across_runs, avg_fitness_across_runs + std_fitness_across_runs, alpha=0.2, label="Standard Deviation")
+        plt.xlabel("Generation")
+        plt.ylabel("Fitness")
+        plt.title("Fitness over 10 runs")
+        plt.legend(loc="best")
+        plt.grid(True)
+        plt.show()
+
+
+    # run the best found solution in the previous experiment
+    # IMPORTANT to run graphics set HEADLESS to FALSE
+    elif program_name == "run_solution":
+        bsol = np.loadtxt(experiment_name + '/best.txt')
+        print('\n RUNNING SAVED BEST SOLUTION \n')
+        env.update_parameter('speed', 'normal')
+        fitness, playerlife, enemylife, gameruntime = simulation_indu(env, bsol)
+
+        print(f"fitness: {fitness} \nplayerlife: {playerlife}\nenemylife: {enemylife}\ngameruntime: {gameruntime}")
+
+        sys.exit(0)
+
+
+    else:
+        print(f"Program '{program_name}' not found")
+
+
+def run_ea(env, n_hidden_neurons, dom_l, dom_u, npop, cx_prob, mut_prob, n_generations, tournsize):
+
     # Deap only works with evaluation of an individual
     # Cant putt this between other functions because local variable env cant be passed with Deap
     def evaluate_individual(individual):
         fitness = env.play(pcont=np.array(individual))[0]  # Fitness is the first return value of play
         return fitness,
-
-    env.state_to_log()
 
     # number of weights for multilayer with 10 hidden neurons
     n_vars = (env.get_num_sensors() + 1) * n_hidden_neurons + (n_hidden_neurons + 1) * 5
@@ -100,8 +197,6 @@ def main():
     toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=1, indpb=0.05)  # Gaussian mutation
     toolbox.register("select", tools.selTournament, tournsize=tournsize)  # Tournament selec
 
-
-
     # Add statistics
     stats = tools.Statistics(key=lambda ind: ind.fitness.values)
     stats.register("avg", np.mean)
@@ -126,9 +221,8 @@ def main():
     logbook.record(gen=0, nevals=len(population), **record)
     print(logbook.stream)
 
-
     # Evolutionary algorithm
-    for gen in range(n_generations):
+    for gen in range(1, n_generations):
 #        print(f"Generation {gen}")
 
         # Select individuals for the next generation
@@ -153,7 +247,7 @@ def main():
                 del mutant.fitness.values
 
         # Evaluate the new individuals (only those with no fitness values)
-        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+        invalid_ind = [ind for ind   in offspring if not ind.fitness.valid]
         fitnesses = map(toolbox.evaluate, invalid_ind)
         for ind, fit in zip(invalid_ind, fitnesses):
             ind.fitness.values = fit
@@ -176,24 +270,9 @@ def main():
 
         current_best_individual = population[fits.index(best_fitness)]
 
-    # Plot the fitness values over generations
-    gen = logbook.select("gen")
-    avg = logbook.select("avg")
-    std = logbook.select("std")
-    min_fitness = logbook.select("min")
-    max_fitness = logbook.select("max")
+    return logbook, current_best_individual, best_fitness
 
-    plt.figure(figsize=(10, 6))
-    plt.plot(gen, avg, label="Average Fitness")
-    plt.fill_between(gen, np.array(avg) - np.array(std), np.array(avg) + np.array(std), alpha=0.2, label="Standard Deviation")
-    plt.plot(gen, min_fitness, label="Minimum Fitness", color="red")
-    plt.plot(gen, max_fitness, label="Maximum Fitness", color="green")
-    plt.xlabel("Generation")
-    plt.ylabel("Fitness")
-    plt.title("Fitness Evolution Over Generations")
-    plt.legend(loc="best")
-    plt.grid(True)
-    plt.show()
+
 
 
 
