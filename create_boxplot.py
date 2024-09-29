b03f067ae7fe6f9210786d0aceb2f45d30446282
graue-gans <meex.kehrer@gmail.com>
2024-09-29 17:50:51 +0200

    Increase font size

--- create_boxplot.py
+++ create_boxplot.py
@@ -14,19 +14,22 @@ twopointx8 = [88.6, 61.6, 61.6, 88.6, 58.6]
 data = [blendx2, twopointx2, blendx7, twopointx7, blendx8, twopointx8]
 
 fig, ax = plt.subplots(figsize=(10, 6))
-plt.rcParams.update({'font.size': 10})
+plt.rcParams.update({'font.size': 15})
 ax.boxplot(data, showmeans=True)
 
 # Add labels and legend (title is added in latex)
 # plt.xlabel('EA specialist instances')
-plt.ylabel('Individual gain')
+plt.ylabel('Individual gain', fontsize=15)
 
 # Edit x-limits and ticks
 plt.xticks([1, 1.5, 2, 3, 3.5, 4, 5, 5.5, 6],
-           ['BlendX', '\n\n| - - - - - - - - Enemy 2 - - - - - - - - |', '2-PointX',
-            'BlendX', '\n\n| - - - - - - - - Enemy 7 - - - - - - - - |', '2-PointX',
-            'BlendX', '\n\n| - - - - - - - - Enemy 8 - - - - - - - - |', '2-PointX'])
-ax.tick_params(axis='x', which='both',length=0)
+           ['BlendX', '\n\n| - - - - - Enemy 2 - - - - - |', '2-PointX',
+            'BlendX', '\n\n| - - - - - Enemy 7 - - - - - |', '2-PointX',
+            'BlendX', '\n\n| - - - - - Enemy 8 - - - - - |', '2-PointX'])
+ax.tick_params(axis='x', which='both', length=0, labelsize=14)
+ax.tick_params(axis='y', labelsize=14)
 
 plt.tight_layout()
+plt.rcParams.update({'font.size': 15})
+
 plt.show()
