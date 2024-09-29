49917148327bd1ff7d0f525d66f5f525e665d5d6
graue-gans <meex.kehrer@gmail.com>
2024-09-29 17:12:24 +0200

    Adjusted legends

--- Make_graph.py
+++ Make_graph.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 # Load data from logs
 dir_name = 'optimization_test'
-enemy_number = '2'
+enemy_number = '8'
 data_2px = np.load('cx_' + enemy_number + '/logs1.npz')
 data_blendx = np.load('blend_' + enemy_number + '/logs1.npz')
 
@@ -19,22 +19,22 @@ plt.rcParams.update({'font.size': 15})
 plt.plot([-5, max(generations)+5], [80, 80], 'k:', alpha=0.4, label='Convergence threshold')
 
 # Plot EA1 max fitness
-plt.plot(generations, data_2px['max_fitness'], label='EA1: Max Fitness', color='blue', linestyle='-')
+plt.plot(generations, data_2px['max_fitness'], label='EA-2PX: Max Fitness', color='blue', linestyle='-')
 plt.fill_between(generations, data_2px['max_fitness'] - data_2px['std_fitness'],
                  data_2px['max_fitness'] + data_2px['std_fitness'], color='blue', alpha=0.2)
 
 # Plot EA1 average fitness
-plt.plot(generations, data_2px['avg_fitness'], label='EA1: Avg Fitness', color='purple', linestyle='--')
+plt.plot(generations, data_2px['avg_fitness'], label='EA-2PX: Avg Fitness', color='purple', linestyle='--')
 plt.fill_between(generations, data_2px['avg_fitness'] - data_2px['std_avg_fitness'],
                  data_2px['avg_fitness'] + data_2px['std_avg_fitness'], color='purple', alpha=0.2)
 
 # Plot EA2 max fitness
-plt.plot(generations, data_blendx['max_fitness'], label='EA2: Max Fitness', color='green', linestyle='-')
+plt.plot(generations, data_blendx['max_fitness'], label='EA-BLX: Max Fitness', color='green', linestyle='-')
 plt.fill_between(generations, data_blendx['max_fitness'] - data_blendx['std_fitness'],
                  data_blendx['max_fitness'] + data_blendx['std_fitness'], color='green', alpha=0.2)
 
 # Plot EA2 average fitness
-plt.plot(generations, data_blendx['avg_fitness'], label='EA2: Avg Fitness', color='orange', linestyle='--')
+plt.plot(generations, data_blendx['avg_fitness'], label='EA-BLX: Avg Fitness', color='orange', linestyle='--')
 plt.fill_between(generations, data_blendx['avg_fitness'] - data_blendx['std_avg_fitness'],
                  data_blendx['avg_fitness'] + data_blendx['std_avg_fitness'], color='orange', alpha=0.2)
 
